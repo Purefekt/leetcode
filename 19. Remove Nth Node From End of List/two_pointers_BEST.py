1c4760cbea14b531a7fdf21bf7d6fb3395994a8b
Purefekt <roonyyo007@gmail.com>
2022-10-17 12:52:50 -0700

    updated ques 19

--- 19. Remove Nth Node From End of List/two_pointers_BEST.py
+++ 19. Remove Nth Node From End of List/two_pointers_BEST.py
@@ -1,3 +1,15 @@
+"""
+TWO_POINTER - ONE PASS
+Add a dummy node to the start of head to avoid edge cases
+set a slow and fast pointer. Move the fast pointer to n+1 position
+Now move slow and fast by 1 till fast is None.
+At this point slow pointer will be at the node just before the node to remove. Set the next node of slow to next.next
+return dummy.next (since the first node was the empty dummy node)
+
+O(L) time. where L is the length of the linked list. We make one pass over all the nodes in L
+O(1) space
+"""
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,27 +17,24 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # add dummy node
+        
         dummy = ListNode()
         dummy.next = head
         
-        # initialize left pointer to 0 and right pointer to n+1
-        left = dummy
-        # code to set right
-        right = dummy
-        right_pointer_index = n+1
-        current_pointer = 0
-        while current_pointer != right_pointer_index:
-            right = right.next
-            current_pointer += 1
+        slow = dummy
+        fast = dummy
         
-        # increment right and left by 1 till right is None. At this point left will be the node right before the node to remove
-        while right:
-            left = left.next
-            right = right.next
+        # move fast to n+1
+        for i in range(n+1):
+            fast = fast.next
         
-        # change left.next to left.next.next. This will remove the node
-        left.next = left.next.next
+        # move both by 1 till fast is none
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        
+        # skip the next element to slow
+        slow.next = slow.next.next
         
         return dummy.next
     
