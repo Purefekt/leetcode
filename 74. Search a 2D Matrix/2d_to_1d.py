532840cef3f026da700c3a2d16df6a6667ffdd03
Purefekt <roonyyo007@gmail.com>
2022-10-27 13:48:40 -0700

    updated ques 74

--- 74. Search a 2D Matrix/2d_to_1d.py
+++ 74. Search a 2D Matrix/2d_to_1d.py
@@ -1,31 +1,32 @@
+"""
+The 2d array can be though of as a 1d array. Since it is sorted we can run binary search
+Need to encode 1d indexes to 2d indexes. row = 1d_index//num_cols. col = 1d_index//num_rows
+O(log(m*n)) time since the search space is of size m*n on which we run binary search
+O(1) space to store l,r,pivot
+"""
+
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        # consider 2d matrix as 1d sorted array. We will need conversion from 1d to 2d indices.
+        m,n = len(matrix), len(matrix[0])
         
-        left = 0
-        right = (len(matrix) * len(matrix[0])) - 1
-        num_elements_row = len(matrix[0])
+        def get_2d_index(index):
+            row = index//n
+            col = index%n
+            return row, col
         
-        while (left <= right):
-            pivot = (left + right) // 2
-            pivot_row, pivot_col = self.get_index(pivot, num_elements_row)
+        l = 0
+        r = m*n - 1
+        
+        while l<=r:
+            pivot = l + (r-l)//2
+            pivot_r, pivot_c = get_2d_index(pivot)
             
-            if target == matrix[pivot_row][pivot_col]:
+            if matrix[pivot_r][pivot_c] == target:
                 return True
-            elif target > matrix[pivot_row][pivot_col]:
-                left = pivot + 1
-            elif target < matrix[pivot_row][pivot_col]:
-                right = pivot - 1
+            elif matrix[pivot_r][pivot_c] > target:
+                r = pivot - 1
+            else:
+                l = pivot + 1
         
         return False
-        
-    
-    def get_index(self, one_d_index, num_elements_row):
-        # row will be one_d_index // num of elements per row
-        row = one_d_index // num_elements_row
-        # col will be one_d_index % num of elements per row
-        col = one_d_index % num_elements_row
-        
-        return row, col
-        
