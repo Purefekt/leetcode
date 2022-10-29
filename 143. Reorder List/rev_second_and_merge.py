4680d815450e237adef1071be8af4ffe0924130b
Purefekt <roonyyo007@gmail.com>
2022-10-29 16:57:24 -0700

    added ques 143

--- 143. Reorder List/rev_second_and_merge.py
+++ 143. Reorder List/rev_second_and_merge.py
@@ -1,3 +1,40 @@
-a = [1,2,3]
+"""
+Find the middle of the list using fast and slow pointer.
+Reverse the 2nd half in place
+merge first and second half one by one.
 
-print(list.top())
+O(N) time. Linear pass to find the middle. n/2 to reverse the second part. n/2 to merge the lists
+O(1) space.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        if not head:
+            return
+        
+        # find the middle of linked list
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+            
+        # reverse the second part of the list
+        # reverse the second half in-place
+        prev, curr = None, slow
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next       
+
+        # merge two sorted linked lists
+        first, second = head, prev
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next
