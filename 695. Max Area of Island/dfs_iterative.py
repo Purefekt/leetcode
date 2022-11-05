6340c4d57839c66fb434195b0728cc09dd9a373b
Purefekt <roonyyo007@gmail.com>
2022-11-04 18:26:48 -0700

    updates ques 695

--- 695. Max Area of Island/dfs_iterative.py
+++ 695. Max Area of Island/dfs_iterative.py
@@ -1,49 +1,50 @@
+"""
+Go through all cells in the matrix
+Run DFS everytime we are at a cell with 1 and get max size
+Use visited set to avoid going to cells again
+O(m*n) time to iterate over all cells
+O(m*n) space to store all cells
+"""
+
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
-        rows = len(grid)
-        cols = len(grid[0])
-        max_island_size = 0
+        m = len(grid)
+        n = len(grid[0])
         
-        for i in range(rows):
-            for j in range(cols):     
-                # if we find a landmass, get its 4 directional land masses
+        visited = set()
+        max_size = 0
+        
+        for i in range(m):
+            for j in range(n):
+                
                 if grid[i][j] == 1:
-                    stack = collections.deque()
-                    stack.append((i,j))
                     curr_island_size = 0
-
+                    
+                    stack = [(i,j)]
+                    visited.add((i,j))
                     while stack:
-
-                        curr_i, curr_j = stack.pop()
-                        # inc island size and change curr island num to 2, to avoid re-adding. Similar to 733 ques
-                        grid[curr_i][curr_j] = 2
+                        r,c = stack.pop()
                         curr_island_size += 1
-                        # add 4 directional IF they are valid and IF they have value 1. Also change the newely added points to 2 to avoid re-adding.
-                        # up
-                        if curr_i-1>=0 and curr_j>=0 and curr_i-1<rows and curr_j<cols and grid[curr_i-1][curr_j] == 1:
-                            stack.append((curr_i-1,curr_j))
-                            grid[curr_i-1][curr_j] = 2
-                        # right
-                        if curr_i>=0 and curr_j+1>=0 and curr_i<rows and curr_j+1<cols and grid[curr_i][curr_j+1] == 1:
-                            stack.append((curr_i,curr_j+1))
-                            grid[curr_i][curr_j+1] = 2
-                        # down
-                        if curr_i+1>=0 and curr_j>=0 and curr_i+1<rows and curr_j<cols and grid[curr_i+1][curr_j] == 1:
-                            stack.append((curr_i+1,curr_j))
-                            grid[curr_i+1][curr_j] = 2
-                        # left
-                        if curr_i>=0 and curr_j-1>=0 and curr_i<rows and curr_j-1<cols and grid[curr_i][curr_j-1] == 1:
-                            stack.append((curr_i,curr_j-1))
-                            grid[curr_i][curr_j-1] = 2
-
-
-                    # update max island size
-                    max_island_size = max(max_island_size, curr_island_size)
-                    # reset curr_island_size
-                    curr_island_size = 0
+                        
+                        # check and add all directions
+                        if r-1>=0 and grid[r-1][c] == 1 and (r-1,c) not in visited:
+                            stack.append((r-1,c))
+                            visited.add((r-1,c))
+                        if c+1<n and grid[r][c+1] == 1 and (r,c+1) not in visited:
+                            stack.append((r,c+1))
+                            visited.add((r,c+1))
+                        if r+1<m and grid[r+1][c] == 1 and (r+1,c) not in visited:
+                            stack.append((r+1,c))
+                            visited.add((r+1,c))
+                        if c-1>=0 and grid[r][c-1] == 1 and (r,c-1) not in visited:
+                            stack.append((r,c-1))
+                            visited.add((r,c-1))
+                        
+                    max_size = max(max_size, curr_island_size)
+                
                 else:
                     continue
-    
-        return max_island_size
-             
+        
+        return max_size
+                        
