e4db78ffea9e7948e53f811bd944b499b61085b8
Purefekt <roonyyo007@gmail.com>
2022-11-06 17:18:33 -0800

    updated ques 167

--- 167. Two Sum II - Input Array Is Sorted/two_pointers_start_and_end.py
+++ 167. Two Sum II - Input Array Is Sorted/two_pointers_start_and_end.py
@@ -1,26 +1,22 @@
 """
-set p1 to left end of the list
-set p2 to right end of the list
-
-if list[p1] + list2[p2], then return those indicies, have to change p2 from neg indicies to normal index
-if the sum is larger than target, then move p2 left, since p1 and any number larger than p1 and p2 will still be larger than target
-similarly if sum is smaller than target, move p1 to right
+Two pointers. l at 0 and r at len-1
+Add nums[l] and nums[r], if it is larger than target, move r left. If it is smaller than move l right.
+If we find the target, return l+1,r+1
+O(n) time to traverse the list once
+O(1) space to store l and r
 """
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        p1 = 0
-        p2 = -1
+        l = 0
+        r = len(numbers)-1
         
-        while(1):
-            if numbers[p1] + numbers[p2] == target:
-                # p2 has neg indicies, must change to normal
-                p2 = p2 + len(numbers)
-                
-                return [p1+1, p2+1,]
-            if numbers[p1] + numbers[p2] > target:
-                p2 -= 1
-            if numbers[p1] + numbers[p2] < target:
-                p1 += 1
+        while l<r:
+            if numbers[l] + numbers[r] > target:
+                r -= 1
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+            else:
+                return [l+1, r+1]
         
