c90219f61373dcf6f062b14c6d22e54141258efe
Purefekt <roonyyo007@gmail.com>
2022-11-07 00:22:11 -0800

    updated ques 215

--- 215. Kth Largest Element in an Array/minheap_O(Nlogk).py
+++ 215. Kth Largest Element in an Array/minheap_O(Nlogk).py
@@ -1,21 +1,23 @@
 """
-Using min heap. Convert all nums to negative and then loop over and pop the min heap k times. The last popped will be the kth largest. 
+Heap
+Multiply all elements by negative to convert min heap functionality to max heap
+Heapify the list
+pop k times, the last popped element will be -result
 
-O(Nlogk)
+O(nlogk) time
+O(k) space to store heap elements
 """
-
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
-        # flip the sign of all num in nums
         for i in range(len(nums)):
             nums[i] = -nums[i]
         
-        # create priority queue of nums
         heapq.heapify(nums)
         
+        res = 0
         for i in range(k):
-            out = heapq.heappop(nums)
+            res = heapq.heappop(nums)
         
-        return -out
+        return -res
     
--- 215. Kth Largest Element in an Array/quickselect_o(n)_avg_o(n^2)_worst.py
+++ 215. Kth Largest Element in an Array/quickselect_o(n)_avg_o(n^2)_worst.py
@@ -1,44 +1,45 @@
 """
-Use quicksort to sort only the pivot element in its correct index.
-set the right most element to the pivot. Go through the array excluding the last element and maintain a pointer variable starting at left end.
-if the current num is larger than the pivot, do nothing, else swap the current element with pointer element and increment pointer by 1.
+Quick_sort. This algorithm will take the pivot element and set it to its correct index in linear time.
+We set the last element as the pivot and use a pointer initialized at the left end.
+Iterate from left end till the end of array except for the pivot element.
+At each iteration, if the ith element is greater than the pivot element, do nothing.
+If the ith element is <= the pivot element, swap ith element and pointer element and increment pointer by 1.
+At the end of the loop, swap pivot element and pointer element. Now the element at the pointer is in its correct index in a ascending order sorted array.
+The kth largest element is also the n-kth element. If this pointer == n-k, this means we found the element. Otherwise run quickselect on either the left array or right array.
+If the pointer > n-k, we shift the right to pivot-1, else we shift left to pointer+1
 
-At the end of this loop, swap the element at pointer index with the pivot element. Now the pivot element is at its sorted index and all elements smaller to it are at its left and all elements larger than it are on its right.
-
-kth largest element is also the len(nums)-k element. Thus if the pivot element (now at pointer index) is equal to len(nums)-k thus we have found it and we can return nums[pointer].
-If len(nums)-k > pointer index, then we run quicksort on the right subarray
-else we run quisort on the left subarray
-
-O(n) average time and O(n^2) worst time. O(1) space since swapping is in place.
+O(n) average time and O(n^2) worst case time.
+O(1) space using in place
 """
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
+        n = len(nums)
+        
         def quick_sort(l,r):
-            pivot = r
             pointer = l
-    
-            for i in range(l, r):
+            pivot = r
+            
+            for i in range(l,r):
                 if nums[i] > nums[pivot]:
                     continue
-                elif nums[i] <= nums[pivot]:
-                    nums[pointer], nums[i] = nums[i], nums[pointer]
+                else:
+                    nums[i], nums[pointer] = nums[pointer], nums[i]
                     pointer += 1
-            # finally swap pivot element and element at pointer position
             nums[pointer], nums[pivot] = nums[pivot], nums[pointer]
-                
-            if len(nums)-k == pointer:
+            
+            if pointer == n-k:
                 return nums[pointer]
-            elif len(nums)-k > pointer:
+            elif pointer < n-k:
                 l = pointer+1
                 return quick_sort(l,r)
-            elif len(nums)-k< pointer:
+            else:
                 r = pointer-1
                 return quick_sort(l,r)
         
         l = 0
-        r = len(nums)-1
+        r = n-1
         
         return quick_sort(l,r)
-    
+        
