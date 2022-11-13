9bbe766d3f45e3377e6f72081a36a982c8ff78ea
Purefekt <roonyyo007@gmail.com>
2022-11-12 18:32:22 -0800

    updated ques 235

--- 235. Lowest Common Ancestor of a Binary Search Tree/compare_p_q_to_node.py
+++ 235. Lowest Common Ancestor of a Binary Search Tree/compare_p_q_to_node.py
@@ -3,6 +3,8 @@ if both p and q are larger than the current node, then go to the right subtree
 if both p and q are smaller than the current node, then go to the left subtree
 if one is less and one is larger, this means the current node is the LCA
 add code for when p or q equals the node itself, or when it is an ancestor of itself
+O(n) time to traverse all nodes in worst case
+O(1) space.
 """
 
 # Definition for a binary tree node.
@@ -15,17 +17,21 @@ add code for when p or q equals the node itself, or when it is an ancestor of it
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        stack = []
+        pval = p.val
+        qval = q.val
         
-        stack.append(root)
+        node = root
         
-        while stack:
-            node = stack.pop()
-            
-            if p.val > node.val and q.val > node.val:
-                stack.append(node.right)
-            if p.val < node.val and q.val < node.val:
-                stack.append(node.left)
-            if (p.val >= node.val and q.val <= node.val) or (p.val <= node.val and q.val >= node.val):
+        while node:
+            if node.val == qval or node.val == pval:
                 return node
-        
+            
+            if node.val > qval and node.val > pval:
+                node = node.left
+            
+            elif node.val < qval and node.val < pval:
+                node = node.right
+            
+            else:
+                return node
+            
