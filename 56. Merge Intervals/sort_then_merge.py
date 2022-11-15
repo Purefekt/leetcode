37fc34553f13c5183fb3b2f3612021b14d7ba3e1
Purefekt <roonyyo007@gmail.com>
2022-11-14 16:42:23 -0800

    updated ques 56

--- 56. Merge Intervals/sort_then_merge.py
+++ 56. Merge Intervals/sort_then_merge.py
@@ -1,42 +1,40 @@
 """
-Sort the interval list. Then take two pairs at a time and see if they are overlapping. If they are then merge and add the new merged interval to output and go to the next 2 pairs by jumping 2 indices.
-If they do not overlap then move to next to pairs where the 2nd element of the previous pair is now the first element of the new pairs. Add the first element of the prev pair to the output list.
-Repeat this till there are no more overlapping pairs, use a flag to detect this.
-"""
+Sort the intervals. Now run a while loop and start with the first interval.
+Save this intervals start and end times
+Run a for loop from the current interval next interval till the last interval
+keep updating the new end time if the current and next intervals overlap.
+Move i to j so that we do not explore already merged intervals again.
+Once we reach an interval which does not overlap, break the for loop and append the start and the updated end to res. Continue with next non merged interval
+O(n) time to go over all intervals
+O(logn) space to sort
+""" 
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
-        # edge case where there is only 1 interval
-        if len(intervals) < 2:
-            return intervals
-    
-        # sort intervals
         intervals.sort()
+
+        res = []
+        i = 0
+        while i < len(intervals):
+            
+            start_i = intervals[i][0]
+            end_i = intervals[i][1]
+            
+            for j in range(i+1, len(intervals)):
+                
+                start_j = intervals[j][0]
+                end_j = intervals[j][1]
+                
+                if end_i >= start_j:
+                    end_i = max(end_i, end_j)
+                    i = j
+                else:
+                    break
+            
+            res.append([start_i, end_i])
+            
+            i += 1
         
-        output, flag = self.merge_helper(intervals)        
-        while flag != 0:
-            output, flag = self.merge_helper(output)
-        
-        return output
-     
-    def merge_helper(self, intervals):      
-        flag = 0
-        output = []
-        p1 = 0
-        p2 = 1
-        while p1 < len(intervals)-1:
-            if intervals[p1][1] >= intervals[p2][0]:
-                output.append([min(intervals[p1][0], intervals[p2][0]), max(intervals[p1][1], intervals[p2][1])])
-                flag = 1
-                p1 += 2
-                p2 += 2
-            else:
-                output.append(intervals[p1])
-                p1 += 1
-                p2 += 1
-        # add the last element
-        if p1<len(intervals):
-            output.append(intervals[p1])
-        return output, flag
-    
+        return res
+            
