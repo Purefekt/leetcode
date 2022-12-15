2e24f5204a9cea2d263d6b7e36400eb3f81ae440
Purefekt <roonyyo007@gmail.com>
2022-12-14 17:00:22 -0800

    added ques 77

---
+++ 77. Combinations/backtrack.py
@@ -0,0 +1,25 @@
+"""
+Backtrack.
+
+O(k nchoosek) time
+O(nchoosek) space
+"""
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        res = []
+
+        def backtrack(start, combo):
+            if len(combo) == k:
+                res.append(combo.copy())
+                return
+            
+            for i in range(start, n+1):
+                combo.append(i)
+                backtrack(i+1, combo)
+                combo.pop()
+        
+        backtrack(1, [])
+        return res
+    
