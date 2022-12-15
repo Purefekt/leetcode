e2a38baed2d93f10cbf57a267a46994be7d543c6
Purefekt <roonyyo007@gmail.com>
2022-12-15 15:21:34 -0800

    updated ques 994

--- 994. Rotting Oranges/bfs_level_order.py
+++ 994. Rotting Oranges/bfs_level_order.py
@@ -1,57 +1,59 @@
 """
-level order bfs traversal. increment the minute on each level.
+Run bfs level order.
+Get the 2s in the original grid and run bfs and keep adding in 4 directions
+Increment the time only at every level.
+Add edge cases for when there are no rotting oranges but some fresh oranges or if there are no fresh oranges
+
+O(m*n) time to go through all cells once since we add it to seen
+O(m*n) to store in queue
 """
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
-        rows = len(grid)
-        cols = len(grid[0])
-        
-        # get num of fresh oranges originally
-        num_fresh_oranges_originally = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 1:
-                    num_fresh_oranges_originally += 1
-        # if num fresh oranges originally is 0, then return 0
-        if num_fresh_oranges_originally == 0:
-            return 0
-        
-        # add all the cells with a rotten orange to a queue
-        queue = collections.deque()
-        for i in range(rows):
-            for j in range(cols):
+
+        m = len(grid)
+        n = len(grid[0])
+
+        # create initial queue with all rotten oranges. Also maintain a set of coords of all 1s
+        queue = []
+        fresh_oranges = set()
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 2:
                     queue.append((i,j))
-        # if queue is empty, means no rotten oranges on the grid, return -1
-        if not queue:
+                elif grid[i][j] == 1:
+                    fresh_oranges.add((i,j))
+        
+        # if the queue is empty and fresh_oranges is not, this means there are only fresh oranges, return -1
+        if not queue and fresh_oranges:
             return -1
+        # if fresh_oranges is empty, this means time taken is 0
+        if not fresh_oranges:
+            return 0
         
-        # go over all rotten oranges at a level and add all rotten oranges in the next level. inc minutes at each level
-        minutes = -1
+        directions = [(-1,0), (0,1), (1,0), (0,-1)]
+        time = -1
+        seen = set()
         while queue:
-            minutes += 1
-            
-            elements_in_curr_level = len(queue)
-            directions = [(-1,0), (0,1), (1,0), (0,-1)]
-            for i in range(elements_in_curr_level):
-                cell_i, cell_j = queue.popleft()
-                # all 4 directions for each cell
-                for dirs in directions:
-                    new_i = cell_i + dirs[0]
-                    new_j = cell_j + dirs[1]
-                    # validity
-                    if new_i>=0 and new_j>=0 and new_i<rows and new_j<cols and grid[new_i][new_j]==1:
-                        grid[new_i][new_j] = 2
-                        # add this new rotten orange to the queue
-                        queue.append((new_i, new_j))
-            
+            time += 1
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                seen.add(node)
+                for direction in directions:
+                    destination = tuple(map(lambda x,y:x+y, node, direction))
+                    new_r, new_c = destination[0], destination[1]
+                    if 0<=new_r<m and 0<=new_c<n:
+                        if destination not in seen:
+                            seen.add(destination)
+                            if grid[new_r][new_c] == 1:
+                                queue.append(destination)
+                                grid[new_r][new_c] = 2
         
-        # after the while loop ends, check the grid, if there is any fresh orange left, return -1
-        for i in range(rows):
-            for j in range(cols):
+        # go through the grid, if it still has any 1, this means all oranges were not affected
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 1:
                     return -1
-        # if for loop ends without return, this means no fresh oranges were left. Return minutes
-        return minutes
+        
+        return time
+        
