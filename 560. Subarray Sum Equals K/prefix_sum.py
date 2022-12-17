1b5f15f3eda2c767a36ca2a1099698b676e8e416
Purefekt <roonyyo007@gmail.com>
2022-12-17 01:37:20 -0800

    added ques 560

--- 560. Subarray Sum Equals K/prefix_sum.py
+++ 560. Subarray Sum Equals K/prefix_sum.py
@@ -1,29 +1,30 @@
 """
-Solve with PrefixSum. O(n) time and space
+Prefixsum
+Create a hashmap with the frequency of prefixsum. We will start with 0:1 since the sum 0 appears once by default even before starting the array
+Go through the numbers and keep adding to the sum
+On each iter, if sum-k is in hashmap, add the count of hashmap[sum-k] to count
+Next if sum is not in the hashmap, add it with freq of 1 else increment by 1
 
-go through the array and keep adding the numbers. On each iteration check if the current sum-k exists in the prefixsum hashmap. If it does then add the number of times it appears to the count.
-
-After that add the current sum to the hashmap. If the current sum is unique then add it with count 1, else increment the count
+O(n) time to go through all elements once
+O(n) space to map n elements
 """
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        prefixsum_map = {0:1}
+        hashmap = {0:1}
+        
         count = 0
         summ = 0
-        for i in range(len(nums)):
-            summ += nums[i]
+        for n in nums:
+            summ += n
+
+            if summ-k in hashmap:
+                count += hashmap[summ-k]
             
-            # check if sum-k appears in hashmap
-            if (summ-k) in prefixsum_map.keys():
-                count += prefixsum_map[summ-k]
-            
-            # add the current sum to hashmap
-            if summ not in prefixsum_map.keys():
-                prefixsum_map[summ] = 1
+            if summ not in hashmap:
+                hashmap[summ] = 1
             else:
-                prefixsum_map[summ] += 1
+                hashmap[summ] += 1
         
         return count
-    
