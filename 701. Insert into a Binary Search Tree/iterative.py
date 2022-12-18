1fff6d3d92aa852391e1b1b453bb5b21bd84a482
Purefekt <roonyyo007@gmail.com>
2022-12-17 21:55:03 -0800

    added ques 701

--- 701. Insert into a Binary Search Tree/iterative.py
+++ 701. Insert into a Binary Search Tree/iterative.py
@@ -1,3 +1,12 @@
+"""
+Use a dummy node to taverse the bst.
+If the val is smaller than the node, go left else go right
+Check if the direction we want to go exists of not, if it does then go else place the val there and break
+
+O(h) where h is height. In average case it is logn and worst case it is n (suppose root is smallest and we keep adding only right children)
+O(1) space since we only use constant space for dummy pointer
+"""
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,30 +15,24 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        
-        # initialize node
-        new_node = TreeNode(val)
-        
-        # edge case
+
         if not root:
-            return new_node
+            return TreeNode(val)
         
-        stack = collections.deque()
-        stack.append(root)
+        dummy = root
+        while dummy:
+            if val < dummy.val:
+                if dummy.left:
+                    dummy = dummy.left
+                else:
+                    dummy.left = TreeNode(val)
+                    break
+            elif val > dummy.val:
+                if dummy.right:
+                    dummy = dummy.right
+                else:
+                    dummy.right = TreeNode(val)
+                    break
         
-        while stack:
-            node = stack.pop()
-            
-            if new_node.val > node.val:
-                if not node.right:
-                    node.right = new_node
-                    return root
-                else:
-                    stack.append(node.right)
-            
-            if new_node.val < node.val:
-                if not node.left:
-                    node.left = new_node
-                    return root
-                else:
-                    stack.append(node.left)
+        return root
+        
