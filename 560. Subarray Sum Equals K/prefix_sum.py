038224d1bf02b947d118b576754bcd3624e196d4
Purefekt <roonyyo007@gmail.com>
2022-12-26 01:34:08 -0800

    updated ques 560

--- 560. Subarray Sum Equals K/prefix_sum.py
+++ 560. Subarray Sum Equals K/prefix_sum.py
@@ -1,9 +1,9 @@
 """
-Prefixsum
-Create a hashmap with the frequency of prefixsum. We will start with 0:1 since the sum 0 appears once by default even before starting the array
-Go through the numbers and keep adding to the sum
-On each iter, if sum-k is in hashmap, add the count of hashmap[sum-k] to count
-Next if sum is not in the hashmap, add it with freq of 1 else increment by 1
+Prefix sum
+Maintain a hashmap of frequency of each prefix sum. Initiate it with 0:1, which means prefixsum of 0 occurs once since when there is no array, the sum is 0
+Start a loop over all nums, for each iteration update the prefix sum.
+Check if prefixsum - k is in the psum_freq hashmap, if it is then add the frequency of prefixsum-k to the count
+Then either add the prefixsum to the hashmap (if it doesnt exist) with a value of 1 or increment its value by 1 (if it already exists)
 
 O(n) time to go through all elements once
 O(n) space to map n elements
@@ -12,19 +12,20 @@ O(n) space to map n elements
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        hashmap = {0:1}
-        
+        psum_freq = {0:1}
         count = 0
-        summ = 0
-        for n in nums:
-            summ += n
 
-            if summ-k in hashmap:
-                count += hashmap[summ-k]
-            
-            if summ not in hashmap:
-                hashmap[summ] = 1
+        prefix_sum = 0
+
+        for n in nums:
+            prefix_sum += n
+
+            if prefix_sum - k in psum_freq:
+                count = count + psum_freq[prefix_sum - k]
+
+            if prefix_sum not in psum_freq:
+                psum_freq[prefix_sum] = 1
             else:
-                hashmap[summ] += 1
+                psum_freq[prefix_sum] += 1
         
         return count
