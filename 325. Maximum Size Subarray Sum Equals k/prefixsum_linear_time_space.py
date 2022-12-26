09e07558f2c4c1410aaa937f24695b6ab3a2fc11
Purefekt <roonyyo007@gmail.com>
2022-12-26 01:18:29 -0800

    added ques 325

--- 325. Maximum Size Subarray Sum Equals k/prefixsum_linear_time_space.py
+++ 325. Maximum Size Subarray Sum Equals k/prefixsum_linear_time_space.py
@@ -1,32 +1,31 @@
 """
-O(N) time and space. Solve with prefixsum
+Prefix sum
+Build a prefix sum hashmap by iterating nums, one by one. Key=prefix sum : value=index
+Initiate the prefix sum map with 0:-1. This means the prefix sum is 0 at index -1. this is to avoid edge cases
+Start loop over n, for each iteration update the current prefix sum and add it to the hashmap if that value doesnt exist
+Note, if a prefix sum is already in the hashmap, do not add it since we need to maxmimize the substring so we need the left most index
+In each iteration check if prefix sum[i] - k is in the hashmap. If it is then we found a valid substring of length i-prefix_sum[cur_psum-k].
+This is because for an array of prefix sums, psum[j] - psum[i] = k, we rearrange this
 
-get the prefixsum for all arrays from 0 to i. On each iteration check if prefixsum-k appears in the indicies dict.
-indicies dict = {prefixsum:index}
-If it appears then update longestsubarray to max(longestsubarray, i - indicies[prefixsum-k])
-add the current prefix sum and its index to the indicies hashmap only if it doesnt exist. 
-NOTE: if we need to find the shortest subarray, then we update the hashmap everytime and use min.
-"""
+O(n) time
+O(n) space
+""" 
 
 class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
         
-        prefixsum = 0
-        indicies = {}
-        longestsubarray = 0
+        prefix_sum = {0:-1}
+
+        longest_len = 0
+        cur_psum = 0
+
+        for i,n in enumerate(nums):
+            cur_psum += n
+            if cur_psum not in prefix_sum:
+                prefix_sum[cur_psum] = i
+
+            if cur_psum - k in prefix_sum:
+                cur_ss_len = i-prefix_sum[cur_psum-k]
+                longest_len = max(longest_len, cur_ss_len)
         
-        for i in range(len(nums)):
-            prefixsum += nums[i]
-            
-            # if the prefixsum till i ==k, then it is by default the longestsubarray till length i
-            if prefixsum == k:
-                longestsubarray = i+1
-            
-            if prefixsum-k in indicies.keys():
-                longestsubarray = max(longestsubarray, i-indicies[prefixsum-k])
-                
-            if prefixsum not in indicies.keys():
-                indicies[prefixsum] = i
-        
-        return longestsubarray
-    
+        return longest_len
