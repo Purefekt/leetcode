95f0aec572321ba9c5f42fd9672bcec06c50915a
Purefekt <roonyyo007@gmail.com>
2023-01-21 19:24:38 -0800

    updated ques 93

--- 93. Restore IP Addresses/backtrack.py
+++ 93. Restore IP Addresses/backtrack.py
@@ -1,10 +1,10 @@
 """
-Backtrack and place dots and see if it is valid
-If the input string has more than 12 chars, this can never be a valid ip address
-The recursive function will take the index after which we want to put a dot, number of dots already used and the current string formed
-For each index, we can place the dot either after s[index:index+1], s[index:index+2], s[index:index+3], thus we run a for loop 3 times for this.
-NOTE: it can be that we might go beyond the length of s when trying to add the dot, thats why we will go till min(index+3, len(s)) to remain in bounds
-Also check for the validity of the integer chunk, it must be <256 and numbers like 002 or 022 are invalid due to leading zeros. Handle this with checking the length of the string version and integer version.
+If len(s) is greater than 12, we can never make a valid ip address, return []
+Backtracking. The recursive func will take current combo, number of dots and index.
+If the number of dots == 4 and the index == len(s), this means we have placed all dots and used all chars, this is a valid ip.
+We need to add either 1 char, 2 chars or 3 chars to the combination. Adding 1 char is done without any conditions
+To add 2 chars, we first check if index+1 exists in the length, if yes then we check if the first char must not be a '0', since '01' is invalid.
+To add 3 chars, we check if index+2 is in the length, if yes then we check if the first char must not be '0' and first 2 chars must not be '00' and the number must not be larger than 255.
 
 O(m^n) time or O(1) since here m=3 and n=4. The height of the decision tree will be at max 4 and each time we have 3 decisions. 
 O(m*n) for stack size
@@ -14,25 +14,33 @@ class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
 
         if len(s) > 12:
-            return
-
+            return []
+        
         res = []
-        def backtrack(idx, dots, cur_ip):
-            # if we have placed 4 dots and index is at the end
+        def backtrack(combo, dots, idx):
             if dots == 4 and idx == len(s):
-                res.append(cur_ip[:-1])
+                res.append('.'.join(combo.copy()))
                 return
             
-            # if we exceed 4 dots but have not reached the end of the string, its invalid
-            if dots > 4:
+            if idx == len(s):
                 return
+            
+            # add 1 char
+            combo.append(s[idx:idx+1])
+            backtrack(combo, dots+1, idx+1)
+            combo.pop()
 
-
-            for j in range(idx, min(idx+3, len(s))):
-                # check if its a valid number
-                cur_integer = int(s[idx:j+1])
-                if cur_integer < 256 and len(s[idx:j+1]) == len(str(cur_integer)):
-                    backtrack(j+1, dots+1, cur_ip + s[idx:j+1] + '.')
+            # add 2 chars
+            if idx < len(s)-1 and s[idx] != '0':
+                combo.append(s[idx:idx+2])
+                backtrack(combo, dots+1, idx+2)
+                combo.pop()
+            
+            # add 3 chars
+            if idx < len(s)-2 and s[idx] != '0' and s[idx:idx+2] != '00' and int(s[idx:idx+3]) < 256:
+                combo.append(s[idx:idx+3])
+                backtrack(combo, dots+1, idx+3)
+                combo.pop()
         
-        backtrack(0,0,'')
+        backtrack([],0,0)
         return res
