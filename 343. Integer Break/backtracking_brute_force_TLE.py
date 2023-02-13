aacbc2d810141404814e0380dbcd1337cfb82683
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2023-02-12 19:41:04 -0800

    added ques 343

---
+++ 343. Integer Break/backtracking_brute_force_TLE.py
@@ -0,0 +1,27 @@
+﻿"""
+Backtracking brute force TLE
+We start with current product as 1, Suppose we have n=4, at the root of the tree we can multiply it with 1 or 2 or 3. (we cannot multiply it with 4 since that would mean we are not breaking the integer at all)
+If we multiply with 1, we are left with the integer 3 to break. If we multiply with 2, we are left with integer 2 to break and so on
+The recursive function takes the current product and remaining.
+Base case is if remaining == 0, add the current_prod to the array res
+We iterate from 1 till (inclusive) the remaining. NOTE: we go till min(n, remaining+1) to avoid going to n itself in the very first time. Return max value from res array
+
+O(2^n) time to explore the entire tree
+O(n) space for the stack
+"""
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+
+        res = []
+        def backtrack(cur_prod, remaining):
+
+            if remaining == 0:
+                res.append(cur_prod)
+                return
+            
+            for i in range(1, min(n, remaining+1)):
+                backtrack(cur_prod * i, remaining-i)
+        
+        backtrack(1, n)
+        return max(res)
---
+++ 343. Integer Break/backtracking_with_memoization.py
@@ -0,0 +1,32 @@
+﻿"""
+Backtracking with memoization
+We can cache the (cur_prod, remaining) tuples in a hashmap, the cache allows us to avoid repeated computation
+Recursive function takes the cur_prod and remaining
+If (cur_prod, remaining) is in memo, then return it
+If remaining == 0 then return cur_prod
+Backtrack in the same way by going from 1 -> min(n, remaining+1). At each iteration get the max val possible. Once the iteration ends, set the value in memo hashmap and return the value
+
+O(n^2) time. We will call the recursive function at most n times and each call will run a for loop for n time
+O(n) space taken by the memo hashmap and stack
+"""
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+
+        memo = {}
+        def backtrack(cur_prod, remaining):
+
+            if (cur_prod, remaining) in memo:
+                return memo[(cur_prod, remaining)]
+
+            if remaining == 0:
+                return cur_prod
+            
+            res = 1
+            for i in range(1, min(n, remaining+1)):
+                res = max(res, backtrack(cur_prod * i, remaining-i))
+            memo[(cur_prod, remaining)] = res
+            return res
+        
+        backtrack(1, n)
+        return  memo[(1,n)]
