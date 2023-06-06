421d0a0748f41301484216ec5d02b4ab4cfd1d40
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2023-06-05 17:25:43 -0700

    added ques 42

---
+++ 42. Trapping Rain Water/2_pointer_BEST.py
@@ -0,0 +1,36 @@
+﻿"""
+At a point, we only need to know the min boundary of either side to find the units of water to trap at an index.
+We can use a left pointer and right pointer, left init at 0 and right init at right end.
+Keep track of the max value at both ends
+If the maxL <= maxR, then we will update the result with the index at l.
+Else, update the result with the index at r.
+Move the pointer we updated, l+=1 or r-=1 and update the maxL or maxR
+
+O(n) time for 1 pass over the height array
+O(1) space to store the l,r,maxL,maxR in constant space
+"""
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        l = 0
+        r = len(height)-1
+        maxL = height[l]
+        maxR = height[r]
+
+        res = 0
+        while l<r:
+            if maxL <= maxR:
+                water = maxL - height[l]
+                if water > 0:
+                    res += water
+                l += 1
+                maxL = max(maxL, height[l])
+            else:
+                water = maxR - height[r]
+                if water > 0:
+                    res += water
+                r -= 1
+                maxR = max(maxR, height[r])
+        
+        return res
---
+++ 42. Trapping Rain Water/using_2_arrays.py
@@ -0,0 +1,36 @@
+﻿"""
+Find the amount of water trapped at each index
+At a given index, the water we can trap is the min(max_left_bound, max_right_bound) - height[idx]
+In the example, we can trap 0 water in idx 1 since the left bound is 0
+In the example, we can trap 2 units at idx 5 since max_left is 2 and max right is 3, thus atleast 2 units can be trapped. That index itself has a height of 0 which means we can utilize all the space.
+
+O(n) time for 3 passes over the height array.
+O(n) space to store the max_left and max_right arrays
+"""
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        max_left = []
+        max_right = [0] * len(height)
+
+        for i in range(len(height)):
+            if i == 0:
+                max_left.append(0)
+            else:
+                max_left.append(max(max_left[i-1], height[i-1]))
+        
+        for i in range(len(height)-1, -1, -1):
+            if i == len(height)-1:
+                max_right[i] = 0
+            else:
+                max_right[i] = max(max_right[i+1], height[i+1])
+        
+        # amount of water on each idx will be the min of both sides - height of that idx
+        res = 0
+        for i in range(len(height)):
+            water = min(max_left[i], max_right[i]) - height[i]
+            if water > 0:
+                res += water
+        
+        return res
