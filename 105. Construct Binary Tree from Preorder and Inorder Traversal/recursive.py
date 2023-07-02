fee154c5e1991123199e15859bfcf2afe2214844
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2023-07-02 10:29:15 -0700

    updated ques 105

--- 105. Construct Binary Tree from Preorder and Inorder Traversal/recursive.py
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal/recursive.py
@@ -1,9 +1,11 @@
 """
-The first element in preorder is the root of the tree. In [3,9,20,15,7], 3 is the root.
-All the nodes to the left of 3 in the 'inorder' traversal belong to the left subtree and all the nodes to the right of 3 belong to the right subtree.
-Since inorder is [9,3,15,20,7], [9] is the leftsubtree and [15,20,7] is the right subtree inorder traversals. Using the index of 3, we can get the preorder left and right.
-preorder left is [9] and preorder right is [20,15,7].
-Pass these values to the recursive call, everytime set root to TreeNode(preorder[0]), find mid in inorder and get the values
+The first element of the preorder list will be the root of the tree.
+The elements to the left of this element in the "inorder" list will lie in its left subtree, and the elements to the right of this element in "inorder" list will lie in its right subtree.
+preorder = [3,9,20,15,7], the root is 3.
+inorder = [9,3,15,20,7], [9] lies in the left subtree and [15,20,7] lie in the right.
+Repeat this recursively for each node. Helper function can take the preorder list, inorder list and current node.
+Set the node.val to preorder[0].
+Find the index of preorder[0] in inorder, and create left_preorder, left_inorder, right_preorder, right_inorder lists.
 
 O(n) time to go over all nodes
 O(n) space, since implicit stack will take O(n) in worst case and O(logn) in best case
@@ -17,14 +19,25 @@ O(n) space, since implicit stack will take O(n) in worst case and O(logn) in bes
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        
-        if not preorder or not inorder:
-            return None
-        
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
 
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        def helper(p, i, node):
+            node.val = p[0]
 
+            idx = i.index(p[0])
+
+            left_p = p[1:1+idx]
+            left_i = i[:idx]
+
+            right_p = p[1+idx:]
+            right_i = i[idx+1:]
+
+            if left_p:
+                node.left = TreeNode()
+                helper(left_p, left_i, node.left)
+            if right_p:
+                node.right = TreeNode()
+                helper(right_p, right_i, node.right)
+        
+        root = TreeNode()
+        helper(preorder, inorder, root)
         return root
