36c00e28861c68d1c8be750ff034ff6657bf4012
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2023-07-02 18:51:41 -0700

    updated ques 21

--- 21. Merge Two Sorted Lists/iteration.py
+++ 21. Merge Two Sorted Lists/iteration.py
@@ -1,8 +1,10 @@
 """
-Iteration
-Make a false head. Use a pointer tail to iterate over and build the new linked list.
-Check each node of list1 and list2. point to whichever is smaller.
-Once the while loop terminates, one of the two lists are empty, point the end of the tail to the other list.
+Create a new head and use pointers at list1 and list2.
+Go through the lists and add the smaller and move on.
+At the end check if either of the lists remain, then add it to the new list.
+
+O(m+n) time to go through all nodes in both lists.
+O(1) space since we just use pointers.
 """
 
 # Definition for singly-linked list.
@@ -13,27 +15,25 @@ Once the while loop terminates, one of the two lists are empty, point the end of
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        # start with dummy node
-        dummy = ListNode()
-        tail = dummy
-        
-        while list1 and list2:
-            # if list1 node is smaller, point to it. update tail and list1
-            if list1.val < list2.val:
-                tail.next = list1
-                tail = tail.next
-                list1 = list1.next
-            # else if list2 node is smaller or equal, point to it. update tail and list2
+        head = ListNode()
+        dummy = head
+        d1 = list1
+        d2 = list2
+
+        while d1 and d2:
+            if d1.val < d2.val:
+                dummy.next = d1
+                d1 = d1.next
             else:
-                tail.next = list2
-                tail = tail.next
-                list2 = list2.next
+                dummy.next= d2
+                d2 = d2.next
+            
+            dummy = dummy.next
         
-        # add the list which isnt null yet
-        if list1:
-            tail.next = list1
-        else:
-            tail.next = list2
+        if d1:
+            dummy.next = d1
+        if d2:
+            dummy.next = d2
+
+        return head.next
         
-        return dummy.next
-    
