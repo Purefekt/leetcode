5bc056bf9f00bcfd4a20c44d1ab2e99ea4ca72e8
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2023-08-22 15:24:30 -0700

    added ques 1921

---
+++ 1921. Eliminate Maximum Number of Monsters/greedy_and_sorting.py
@@ -0,0 +1,24 @@
+﻿"""
+Greedy and sorting.
+Get an array called minute reaches where each index belongs to a monster and what time that monster will reach the town. 
+Time taken = distance / time.
+Sort this to eliminate the monsters in the order they arrive to the town.
+Now create a simulation from time=0 till number of monsters, if the current time is >= current monster's reach time, this means we cannot kill it and we lose, return.
+"""
+
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        
+        minuteReaches = []
+        for i in range(len(dist)):
+            minuteReaches.append(math.ceil(dist[i]/speed[i]))
+
+        res = 0
+        minuteReaches.sort()
+        for i in range(len(minuteReaches)):
+            if i >= minuteReaches[i]:
+                return res
+            res += 1
+        
+        return res
+        
