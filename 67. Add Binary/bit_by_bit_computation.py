9a9f9398afac66a5ca75ec5a5fe71a4843881282
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2024-03-30 23:21:56 -0700

    updated ques 67

--- 67. Add Binary/bit_by_bit_computation.py
+++ 67. Add Binary/bit_by_bit_computation.py
@@ -1,43 +1,31 @@
-"""
-Bit by bit computation without changing into int
-"""
-
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         
-        # add leading zeros to the smaller string
-        if len(a) > len(b):
-            num_zeros = len(a) - len(b)
-            b = '0'*num_zeros + b
-        elif len(b) > len(a):
-            num_zeros = len(b) - len(a)
-            a = '0'*num_zeros + a
+        # always keep a larger
+        if len(b) > len(a):
+            a,b = b,a
         
-        carry = '0'
-        output = []
-        for i in range(len(a)):
-            op1 = a[-(i+1)]
-            op2 = b[-(i+1)]
-            
-            if op1 == '0' and op2 == '0' and carry == '0':
-                output.append('0')
-            elif op1 != op2 and carry == '0':
-                output.append('1')
-            elif op1 == '1' and op2 == '1' and carry == '0':
-                output.append('0')
-                carry = '1'
-            elif op1 != op2 and carry == '1':
-                output.append('0')
-                carry = '1'
-            elif op1 == '1' and op2 == '1' and carry == '1':
-                output.append('1')
-                carry = '1'
-            elif op1 == '0' and op2 == '0' and carry == '1':
-                output.append('1')
-                carry = '0'
+        b = "0" * (len(a)-len(b)) + b
+
+        carry = 0
+        res = ""
+        for i in range(len(a)-1, -1, -1):
+            val = int(a[i]) + int(b[i]) + carry
+            if val == 0:
+                res += '0'
+                carry = 0
+            elif val == 1:
+                res += '1'
+                carry = 0
+            elif val == 2:
+                res += '0'
+                carry = 1
+            elif val == 3:
+                res += '1'
+                carry = 1
         
-        if carry == '1':
-            output.append('1')
-        output.reverse()
-        return ''.join(output)
-    
+        if carry == 1:
+            res += '1'
+        
+        res = res[::-1]
+        return res
