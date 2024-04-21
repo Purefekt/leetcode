fff3285fd5bae80d5007aca5c3a5dd7de0c76b3b
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2024-04-20 17:04:50 -0700

    added ques 1507

---
+++ 1507. Reformat Date/easy.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        
+        months = {
+            'Jan': '01',
+            'Feb': '02',
+            'Mar': '03',
+            'Apr': '04',
+            'May': '05',
+            'Jun': '06',
+            'Jul': '07',
+            'Aug': '08',
+            'Sep': '09',
+            'Oct': '10',
+            'Nov': '11',
+            'Dec': '12'
+        }
+
+        date = date.split(' ')
+        date[-3] = date[-3][:-2]
+        if len(date[-3]) == 1:
+            date[-3] = '0' + date[-3]
+        res = date[-1] + '-' + months[date[-2]] + '-' + date[-3]
+
+        return res
