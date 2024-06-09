26aebb5fc7693ec6039ea8ba2e553a996a099270
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2024-06-09 00:43:15 -0700

    update ques 974

--- 974. Subarray Sums Divisible by K/prefixsum.py
+++ 974. Subarray Sums Divisible by K/prefixsum.py
@@ -1,32 +1,31 @@
 ﻿"""
-Prefixsum
-Create the prefixsum for the array nums.
-Create a pmap with {0:1},
-Go through the psum array, for each num, check if num%k is in the pmap. If it is then add that value (frequency) to count
-Next increment the frequency of that num%k, if it is the first time then add it to the map with 1
+Prefix sum mod.
+Get the mod k prefix sum of the array.
+Also add a 0 at the start, for example for [1,2,3] k=2, we get psum as [0, 1, 3, 6] and psum mod k as [0, 1, 1, 0].
+Now iterate through psum mod, and track the frequency of each number.
+If a number exists in frequency, add its count to result and then increment 1.
+If it does not exist, add the key value pair number, 1 to the hashmap.
 
-O(n) time to create prefix sum and to go over it again
-O(n) space to store the psum%k values in pmap hashmap
+O(n) time to build the psum array and one more pass over it.
+O(n) space used by psum array and freq hashmap.
 """
 
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-
-        pmap = {0:1}
-
-        psum = []
-        psum_i = 0
-        for num in nums:
-            psum_i += num
-            psum.append(psum_i)
         
-        count = 0
-        for num in psum:
-            if num%k in pmap:
-                count += pmap[num%k]
-                pmap[num%k] += 1
+        psum = [0]
+        cur = 0
+        for n in nums:
+            cur += n
+            psum.append(cur%k)
+        
+        freq = {}
+        res = 0
+        for n in psum:
+            if n in freq:
+                res += freq[n]
             else:
-                pmap[num%k] = 1
+                freq[n] = 0
+            freq[n] += 1
         
-        return count
-        
+        return res
