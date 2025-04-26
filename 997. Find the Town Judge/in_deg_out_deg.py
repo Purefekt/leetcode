8bf5db801bbeb812779b3defdbf50962f4e839ae
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2025-04-25 20:02:07 -0700

    update ques 997

--- 997. Find the Town Judge/in_deg_out_deg.py
+++ 997. Find the Town Judge/in_deg_out_deg.py
@@ -1,22 +1,17 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         
-        # in deg and out deg
+        # get in degree and out degree of all nodes
         in_deg = {i:0 for i in range(1, n+1)}
         out_deg = {i:0 for i in range(1, n+1)}
-        for src, dst in trust:
-            in_deg[dst] += 1
-            out_deg[src] += 1
+
+        for a,b in trust:
+            in_deg[b] += 1
+            out_deg[a] += 1
         
-        candidate = None
-        for k,v in in_deg.items():
-            if v == n-1:
-                candidate = k
-                break
-        
-        if not candidate:
-            return -1
-        
-        if out_deg[candidate] == 0:
-            return candidate
+        # go through in degree, all nodes must have 0 in degree and 1 must have in degree n-1
+        for node in in_deg:
+            if in_deg[node] == n-1 and out_deg[node] == 0:
+                return node
+
         return -1
