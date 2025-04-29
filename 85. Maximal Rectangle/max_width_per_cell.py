9767efe6a2105448536ab205eb2f32f5a6cbbc5a
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2025-04-28 21:49:40 -0700

    update ques 85

--- 85. Maximal Rectangle/max_width_per_cell.py
+++ 85. Maximal Rectangle/max_width_per_cell.py
@@ -1,61 +1,70 @@
 """
-Get the max width for each cell in the matrix.
-Iterate through each row and for each cell keep adding 1s, if we hit a 0, reset it to 0.
-Now we will have a matrix with max widths for each cell.
-We can calculate the max rectangle at a cell using this info by linearly searching upwards.
-If we have [
-    1,0,1,0,1
+Get the max width for each cell in each row.
+For a matrix like:
+[
+    1,0,0,1,1
     1,0,1,1,1
-    1,1,1,1,1
+    1,1,0,1,1
 ]
-Then the max width matrix will be [
-    1,0,1,0,1
+We get
+[
+    1,0,0,1,2
     1,0,1,2,3
-    1,2,3,4,5
+    1,2,0,1,2
 ]
-Now if we take the bottom right most cell, we go upwards and only care about its own max width column, which is [
-    1
-    3
-    5
-]
-In first iteration, we see that the max width is 5 and height is 1 and so area is 5.
-In second iteration, we see that the max width is 3 and height is 2 and so area is 6.
-In third iteration, we see that the max width is 1 and height is 3 and so area is 3.
-Out of these we track the max area which is 6.
-We repeat this for every single cell.
+Now iterate through all cells.
+For each cell, go top to bottom and see the largest height we can have.
+During this process also track the min height of all the cells we are considering.
+Suppose we are at [1][4], we see that the width is 3 and we set height to 1. Area is 1*3.
+Now we go down and see the cell below us is also not 0, now height is 2 but the width is 2 since we depend on the smallest width. New area is 2*2.
+Note that we would have had an earlier better area when we started at [0][4] 
+    first we get area as 1*2
+    then we get area as 2*2 
+    then we get area as 3*2
 
-O(n^2*m) time since for each cell we perform this calculation for all n rows and we have n*m cells.
-O(n*m) space for the matrix, though we can safely use input matrix for max width matrix and use O(1) space.
+O(m*m*n) since we traverse through the entire matrix and for each cell, go down all the rows.
+O(m*n) space used by width matrix
 """
 
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         
-        m = len(matrix)
-        n = len(matrix[0])
+        m,n = len(matrix), len(matrix[0])
 
-        # get the max width of column for each cell
+        #get max width of each cell
+        width = []
         for i in range(m):
-            matrix[i][0] = int(matrix[i][0])
-            for j in range(1, n):
-                if matrix[i][j] == "0":
-                    matrix[i][j] = 0
-                else:
-                    matrix[i][j] = int(matrix[i][j-1]) + 1
+            row = []
+            for j in range(n):
+                row.append(0)
+            width.append(row)
         
+        for i in range(m):
+            cur = 0
+            for j in range(n):
+                if matrix[i][j] == '0':
+                    cur = 0
+                else:
+                    cur += 1
+                width[i][j] = cur
+        
+        # now we have the maximal width at each cell
+        # we can use this to find the maximum area
+        # iterate through each cell
+        # for each cell, go top to bottom and get the largest height we can while still adding cells which are 1. During this track the min width 
         res = 0
         for i in range(m):
             for j in range(n):
-                min_width = matrix[i][j]
-                res = max(res, min_width)
+                if width[i][j] != 0:
+                    min_width = width[i][j]
+                    res = max(res, min_width)
 
-                # for each cell, go up and get the area of triangles using the min width
-                iteration = 2
-                for k in range(i-1, -1, -1):
-                    min_width = min(min_width, matrix[k][j])
-                    if min_width == 0:
-                        break
-                    res = max(res, min_width * iteration)
-                    iteration += 1
+                    height = 1
+                    k = i+1
+                    while k<m and width[k][j] != 0:
+                        height += 1
+                        min_width = min(min_width, width[k][j])
+                        res = max(res, min_width * height)
+                        k += 1
         
         return res
