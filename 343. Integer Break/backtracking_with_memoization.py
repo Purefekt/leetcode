e2c6dd9729a7da345f79cc8069b6eeba74c3650a
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2025-04-30 20:26:46 -0700

    update ques 343

--- 343. Integer Break/backtracking_with_memoization.py
+++ 343. Integer Break/backtracking_with_memoization.py
@@ -1,32 +1,35 @@
 ﻿"""
-Backtracking with memoization
-We can cache the (cur_prod, remaining) tuples in a hashmap, the cache allows us to avoid repeated computation
-Recursive function takes the cur_prod and remaining
-If (cur_prod, remaining) is in memo, then return it
-If remaining == 0 then return cur_prod
-Backtrack in the same way by going from 1 -> min(n, remaining+1). At each iteration get the max val possible. Once the iteration ends, set the value in memo hashmap and return the value
+DP
+In one step break n into all numbers from 2 -> n. call this k.
+This amount is multiplied to current amount and remaining which is n-k is left over.
+Base case if remaining is 0 or 1, then return 1.
+Note that loop should go at max till n, so take min(remaining+1, n) since in the first call, remaining = n and that will make it go to n+1.
 
-O(n^2) time. We will call the recursive function at most n times and each call will run a for loop for n time
-O(n) space taken by the memo hashmap and stack
+O(n) time.
+O(n) space.
 """
 
 class Solution:
     def integerBreak(self, n: int) -> int:
+        
+        # in one step break n into all numbers from 2 -> n, call this k
+        # this amount is multiplied to current amount and remaining which is n-k is left over
+        # base case if remaining is 0 or 1, then return
 
-        memo = {}
-        def backtrack(cur_prod, remaining):
+        dp = {}
+        def helper(remaining):
+            if remaining in dp:
+                return dp[remaining]
 
-            if (cur_prod, remaining) in memo:
-                return memo[(cur_prod, remaining)]
-
-            if remaining == 0:
-                return cur_prod
+            if remaining == 0 or remaining == 1:
+                return 1
             
             res = 1
-            for i in range(1, min(n, remaining+1)):
-                res = max(res, backtrack(cur_prod * i, remaining-i))
-            memo[(cur_prod, remaining)] = res
+            for k in range(2, min(remaining+1, n)):
+                cur = k * helper(remaining-k)
+                res = max(res, cur)
+            
+            dp[remaining] = res
             return res
         
-        backtrack(1, n)
-        return  memo[(1,n)]
+        return helper(n)
