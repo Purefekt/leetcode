88b9b66081a029cc65189ba65188be01c7f0a21f
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2025-05-04 12:06:42 -0700

    update ques 733

--- 733. Flood Fill/dfs.py
+++ 733. Flood Fill/dfs.py
@@ -1,38 +1,30 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
-        # no change if that pixel is already of the given color
-        if image[sr][sc] == color:
+        # get the original color
+        # start bfs from (sr, sc)
+        # add new cells if they are the new color
+        # update input image so we do not need visited
+
+        m,n = len(image), len(image[0])
+        original_color = image[sr][sc]
+
+        # edge case if original color and color are the same
+        if original_color == color:
             return image
-        
-        # get num of rows and cols
-        rows = len(image)
-        cols = len(image[0])
-        
-        original_col = image[sr][sc]
-        
-        stack = collections.deque()
-        stack.append((sr,sc))
-        
-        while stack:
-            i,j = stack.pop()
-            
-            # change color
+
+        queue = collections.deque()
+        queue.append((sr,sc))
+        directions = [(0,1), (1,0), (0,-1), (-1,0)]
+
+        while queue:
+            node = queue.popleft()
+            i,j = node
             image[i][j] = color
-            
-            # add 4 directional pixels IF they are valid:
-            # up (-1,0)
-            if i-1>=0 and j>=0 and i-1<rows and j<cols and image[i-1][j]==original_col:
-                stack.append((i-1,j))
-            # right (0,1)
-            if i>=0 and j+1>=0 and i<rows and j+1<cols and image[i][j+1]==original_col:
-                stack.append((i,j+1))
-            # down (1,0)
-            if i+1>=0 and j>=0 and i+1<rows and j<cols and image[i+1][j]==original_col:
-                stack.append((i+1,j))
-            # left (0,-1)
-            if i>=0 and j-1>=0 and i<rows and j-1<cols and image[i][j-1]==original_col:
-                stack.append((i,j-1))
+            for d in directions:
+                dest = tuple(map(lambda x,y: x+y, node, d))
+                r,c = dest
+                if 0<=r<m and 0<=c<n and image[r][c] == original_color:
+                    queue.append(dest)
         
         return image
-    
